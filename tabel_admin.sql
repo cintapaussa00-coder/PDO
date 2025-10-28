129adc44af9ef7fca1e3548ad0ab258d962ff0ea
cintapaussa00-coder <cintapaussa00@gmail.com>
2025-10-28 17:02:13 +0800

    Add files via upload

--- tabel_admin.sql
+++ tabel_admin.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Oct 28, 2025 at 08:48 AM
--- Server version: 8.0.30
--- PHP Version: 8.1.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `db_admin`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tabel_admin`
---
-
 CREATE TABLE `tabel_admin` (
   `id` int NOT NULL,
   `nama` varchar(100) NOT NULL,
@@ -34,35 +5,9 @@ CREATE TABLE `tabel_admin` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `tabel_admin`
---
 
 INSERT INTO `tabel_admin` (`id`, `nama`, `email`, `password`) VALUES
 (1, 'Admin Utama', 'admin@example.com', '$2y$10$uG9lzDpFQ9pZCimK7i6s5OTlHFlS6JWhnERoJpknQWnFZ7vTD6vE2');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `tabel_admin`
---
-ALTER TABLE `tabel_admin`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tabel_admin`
---
-ALTER TABLE `tabel_admin`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
